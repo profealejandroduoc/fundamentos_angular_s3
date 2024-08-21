6f54fe58d813be251ba9c94718d8a347402ec9c7
El Profe <al.sepulvedam@profesor.duoc.cl>
2024-08-21 09:25:20 -0400

    Rutas OK

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { AyudaComponent } from './pages/ayuda/ayuda.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'contacto',
+    component:ContactoComponent
+  },
+  {
+    path:'ayuda',
+    component:AyudaComponent
+  },
+  {
+    path:'**',
+    component:NotfoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { AyudaComponent } from './pages/ayuda/ayuda.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactoComponent,
+    AyudaComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
